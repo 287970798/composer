2609ccc0b756ddd1631258ab5aae30f467b2ecdf
287970798 <287970798@qq.com>
2017-08-03 10:24:09 +0800

    创建测试控制器

---
+++ web/controller/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Ren    wechat: yyloon
+ * Date: 2017/8/3 0003
+ * Time: 上午 10:19
+ */
+
+namespace web\controller;
+
+class Index
+{
+    public function show()
+    {
+        echo 'UniteEdu...';
+    }
+}
